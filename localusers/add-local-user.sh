db261e481f78852749959ea19768ba49df8562e6
Sacha Wharton <sacha.wharton@reponsive.co.za>
2020-04-13 16:48:32 +0200

    added test for root, useradd success, passwd success

--- localusers/add-local-user.sh
+++ localusers/add-local-user.sh
@@ -4,7 +4,13 @@
 # You will be prompted for the account name and password.
 
 # Make sure the script is being executed with superuser privileges
-
+USER_NAME=$(id -un)
+if [[ "${UID}" -eq 0 ]]
+then
+  echo 'You are root.'
+else
+  echo 'You are not root.'
+fi
 
 # Ask for the user name.
 read -p 'Enter the username to create: ' USER_NAME
@@ -19,17 +25,29 @@ read -p 'Enter the password to use for the account: ' PASSWORD
 useradd -c "${COMMENT}" -m ${USER_NAME} # quotes are added so that people can have spaces in their names
 
 # Did the useradd command succeed.
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'The useradd command did not execute successfully.'
+  exit 1
+fi
+echo "Your username is ${USER_NAME}"
 
 # Set the password.
 echo ${PASSWORD} | passwd --stdin ${USER_NAME}
 
 # Did the password command succeed.
-
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'The password command did not execute successfully.'
+  exit 1
+fi
+echo "Your password is ${PASSWORD}"
 
 # Force password change on first login.
 passwd -e ${USER_NAME}
 
 # Display the username, password and host where the user was created.
-
+HOSTNAME=$(hostname)
+echo ${USER_NAME} ${PASSWORD} ${HOSTNAME}
 
 
