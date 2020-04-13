8a46a396b2c49437a5616ba895f4efe914b07ace
Sacha Wharton <sacha.wharton@reponsive.co.za>
2020-04-13 16:49:47 +0200

    fixed spacing and typos

---
+++ localusers-vbox/luser-demo02.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Display the UID and username of the user executing this script.
+# Display if the user is the root user or not.
+
+# Display the UID - preset variable
+echo "Your UID is ${UID}"
+
+# Display the username
+USER_NAME=$(id -un)
+# USER_NAME=`id -un` # another of way doing variables
+echo "Your username is ${USER_NAME}"
+
+# Display if the user is the root user or not 
+# Root is always the UID of 0 of any unix system
+# Double brackets is the new way to do if statements
+if [[ "${UID}" -eq 0 ]]
+then
+  echo 'You are root.'
+else
+  echo 'You are not root.'
+fi
+
